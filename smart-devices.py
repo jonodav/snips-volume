c2cb98d8c7013ce1134752c4fb273c34d1764f4c
jonodav <47232848+jonodav@users.noreply.github.com>
2019-07-11 17:26:09 +1000

    Test Version

--- smart-devices.py
+++ smart-devices.py
@@ -5,6 +5,7 @@ from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
+import socket
 
 CONFIG_INI = "config.ini"
 
@@ -32,22 +33,41 @@ class Template(object):
         self.start_blocking()
         
     # --> Sub callback function, one per intent
-    def intent_1_callback(self, hermes, intent_message):
+    def onOffCallback(self, hermes, intent_message):
         # terminate the session first if not continue
         hermes.publish_end_session(intent_message.session_id, "")
         
         # action code goes here...
-        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
+        #print '[Received] intent: {}'.format(intent_message.intent.intent_name)
+
+        # get the slots from intent
+        for (slot_value, slot) in intent_message.slots.items():
+            if slot_value == "device":
+                self.device = slot.first().value.encode("utf8")
+            if slot_value == "state":
+                self.state = slot.first().value.encode("utf8")
+        
+        if self.device == "down lights" or self.device == "ceiling lights" or self.device == "lights":
+            ip = "192.168.0.160"
+            port = 16000
+        
+        if self.state == "on" or self.state == "On":
+            data = "1"
+        if self.state == "off" or self.state == "Off":
+            data = "0"
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+        sock.sendto(data, (ip, port))
 
         # if need to speak the execution result by tts
-        hermes.publish_start_session_notification(intent_message.site_id, "Action1 has been done", "")
+        hermes.publish_start_session_notification(intent_message.site_id, "OnOff intent done", "")
 
-    def intent_2_callback(self, hermes, intent_message):
+    def setBrightnessCallback(self, hermes, intent_message):
         # terminate the session first if not continue
         hermes.publish_end_session(intent_message.session_id, "")
 
         # action code goes here...
-        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
+        #print '[Received] intent: {}'.format(intent_message.intent.intent_name)
 
         # if need to speak the execution result by tts
         hermes.publish_start_session_notification(intent_message.site_id, "Action2 has been done", "")
@@ -57,10 +77,10 @@ class Template(object):
     # --> Master callback function, triggered everytime an intent is recognized
     def master_intent_callback(self,hermes, intent_message):
         coming_intent = intent_message.intent.intent_name
-        if coming_intent == 'intent_1':
-            self.intent_1_callback(hermes, intent_message)
-        if coming_intent == 'intent_2':
-            self.intent_2_callback(hermes, intent_message)
+        if coming_intent == 'thejonnyd:OnOff':
+            self.onOffCallback(hermes, intent_message)
+        if coming_intent == 'thejonnyd:SetBrightness':
+            self.setBrightnessCallback(hermes, intent_message)
 
         # more callback and if condition goes here...
 
